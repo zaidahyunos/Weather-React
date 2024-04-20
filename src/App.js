5e74cd1cf6d7890dd1acbc49b31855ac4996561a
zaidahyunos <zaidah.yunos@gmail.com>
2024-04-20 16:12:23 +0800

    edit title

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Weather React App helloooo</h1>
+        <h1>Weather React App</h1>
         <Weather city="Seoul" />
       </header>
     </div>
